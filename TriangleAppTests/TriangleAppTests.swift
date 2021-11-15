50c67952ccae4dc127a9c0aa2698239251c2d0e4
Galang Aji Susanto <31907731+galangajisusanto@users.noreply.github.com>
2021-11-15 15:15:59 +0700

    added unit testing

--- TriangleAppTests/TriangleAppTests.swift
+++ TriangleAppTests/TriangleAppTests.swift
@@ -1,33 +1,55 @@
-//
-//  TriangleAppTests.swift
-//  TriangleAppTests
-//
-//  Created by Galang Aji Susanto on 15/11/21.
-//
-
+import Foundation
 import XCTest
+
 @testable import TriangleApp
-
 class TriangleAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testInvalidInputSides() {
+        XCTAssertThrowsError(try detectTriangle(-1, -2, -3)) { error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
         }
     }
+    
+    func testDetectEquilateralTriangle() {
+        XCTAssertEqual(try detectTriangle(2, 2, 2), "Segitiga Sama Sisi")
+    }
+    
+    func testDetectIsoscelesTriangle() {
+        XCTAssertEqual(try detectTriangle(1, 1, 2), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(1, 2, 1), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(1, 2, 2), "Segitiga Sama Kaki")
+        XCTAssertEqual(try detectTriangle(2, 1, 2), "Segitiga Sama Kaki")
+    }
+    
+    func testDetectPythagorasTriangle() {
+        XCTAssertEqual(try detectTriangle(6, 8, 10), "Segitiga Siku-Siku")
+    }
+    
+    func detectTriangle(
+        _ sideA: Int,
+        _ sideB: Int,
+        _ sideC: Int
+    ) throws -> String {
+        let sides = [sideA, sideB, sideC].sorted()
+        for side in sides {
+            if side < 0 {
+                throw TriangleError.invalidInput
+            }
+        }
+        if sides[0] == sides[1] && sides[0] == sides[2] {
+            return "Segitiga Sama Sisi"
+        } else if sides[0] == sides[1] || sides[1] == sides[2] {
+            return "Segitiga Sama Kaki"
+        } else if Double((sides[0] * sides[0] + sides[1] * sides[1])).squareRoot() == Double(sides[2]) {
+            return "Segitiga Siku-Siku"
+        } else {
+            return "Segitiga Sembarang"
+        }
+    }
+}
 
+
+
+enum TriangleError: Error {
+    case invalidInput
 }
